22529462c2be7015a8b604c0ebd4fafbccecf7fb
Romans Malinovskis <r@agiletech.ie>
2012-01-20 04:47:57 +0000

    implemented join tests

--- page/dbtest.php
+++ page/dbtest.php
@@ -5,6 +5,8 @@ class page_dbtest extends Page {
 
         // If you want to use database on all pages - move next line into Frontend.php, init() method
         $this->api->dbConnect();
+        $x=$this->api->db->dsql()->expr('show tables')->get();
+        var_dump($x);
 
         $this->add('HtmlElement')
             ->setElement('P')
