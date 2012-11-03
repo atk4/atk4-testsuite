67b519e9f2089f5aeef33267198bc2206c3690d8
Romans Malinovskis <r@agiletech.ie>
2012-11-03 00:46:22 +0000

    New test-cases for dsql and model ordering

--- page/order.php
+++ page/order.php
@@ -1,6 +1,192 @@
 <?php
 class page_order extends Page_DBTest {
-
+    public $proper_responses=array(
+            "Test_order1"=>array (
+  0 => 'select  * from `book`     order by `name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_keyword"=>array (
+  0 => 'select  * from `book`     order by `name` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_wrong_keyword"=>array (
+  0 => 'Exception: Exception_DB: Incorrect ordering keyword (order by=bleh) in :<p id="sample_project_order_p" class="" style=""><a id="sample_project_order_p_view" class="" style="" href="#">More details</a>
+</p>
+',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_arg1"=>array (
+  0 => 'select  * from `book`     order by `name` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_arg2"=>array (
+  0 => 'select  * from `book`     order by `name` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_arg3"=>array (
+  0 => 'select  * from `book`     order by `name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_commas"=>array (
+  0 => 'select  * from `book`     order by `name`, `id` desc, `isbn` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_argcommas"=>array (
+  0 => 'Exception: Exception_DB: If first argument is array, second argument must not be used () in :<p id="sample_project_order_p" class="" style=""><a id="sample_project_order_p_view" class="" style="" href="#">More details</a>
+</p>
+',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_chaining"=>array (
+  0 => 'select  * from `book`     order by `name`, `isbn` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_dots"=>array (
+  0 => 'select  * from `book`     order by `book`.`name` desc, `book`.`isbn` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_dots2"=>array (
+  0 => 'select  * from `book`     order by `book`.`name` desc, `book`.`isbn` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_noexpr"=>array (
+  0 => 'select  * from `book`     order by now() ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_expr"=>array (
+  0 => 'select  * from `book`     order by now() ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_concat"=>array (
+  0 => 'select  * from `book`     order by concat(`book`.`name`, :a, `book`.`isbn`) ',
+  1 => 
+  array (
+    ':a' => ' ',
+  ),
+),
+            "Test_order_field"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_fieldexpr"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by now() ',
+  1 => 
+  array (
+  ),
+),
+            "Test_order_subquery"=>array (
+  0 => 'Exception: Exception_Logic: Method is not defined for this object (class=Model_Author, method=hasMany, arguments=Array) in :<p id="sample_project_order_p" class="" style=""><a id="sample_project_order_p_view" class="" style="" href="#">More details</a>
+</p>
+',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_nofield"=>array (
+  0 => 'Exception: BaseException: Child element not found (Raised by object=Object Model_Book(sample_project_order_model_book_5), element=nosuchfield) in :<p id="sample_project_order_p" class="" style=""><a id="sample_project_order_p_view" class="" style="" href="#">More details</a>
+</p>
+',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_fielquery"=>array (
+  0 => 'select  `name` from `book`     order by `book`.`name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_comma"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name`, `book`.`isbn` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_argcomma"=>array (
+  0 => 'Exception: BaseException: If first argument is array, second argument must not be used (Raised by object=Object Model_Book(sample_project_order_model_book_8)) in :<p id="sample_project_order_p" class="" style=""><a id="sample_project_order_p_view" class="" style="" href="#">More details</a>
+</p>
+',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_argboolean"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_argkeyword"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` desc ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_commakeyword"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`id` desc, `book`.`isbn`, `book`.`name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_exprfield"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by now() ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_expr"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by now() ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_fieldobject"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` ',
+  1 => 
+  array (
+  ),
+),
+            "Test_setorder_chaining"=>array (
+  0 => 'select  *,`book`.`id` from `book`     order by `book`.`name` desc, `book`.`isbn` ',
+  1 => 
+  array (
+  ),
+)
+        );
     function test_order1($t){
         return $t->table('book')->order('name');
     }
@@ -16,6 +202,9 @@ class page_order extends Page_DBTest {
     function test_order_arg2($t){
         return $t->table('book')->order('name',true);
     }
+    function test_order_arg3($t){
+        return $t->table('book')->order('name','asc');
+    }
     function test_order_commas($t){
         return $t->table('book')->order('name,id desc,isbn asc');
     }
@@ -26,10 +215,13 @@ class page_order extends Page_DBTest {
         return $t->table('book')->order('name')->order('isbn','desc');
     }
     function test_order_dots($t){
-        return $t->table('book')->order('book.name,book.isbn');
+        return $t->table('book')->order('book.name desc,book.isbn');
+    }
+    function test_order_dots2($t){
+        return $t->table('book')->order('`book`.name desc , book.`isbn`');
     }
     function test_order_noexpr($t){
-        return $t->table('book')->order('now()'); // error
+        return $t->table('book')->order('now()'); // detects bracket, treats as expression
     }
     function test_order_expr($t){
         return $t->table('book')->order($t->expr('now()'));
@@ -71,7 +263,7 @@ class page_order extends Page_DBTest {
         return $m=$this->add('Model_Book')->setOrder('name',true)->dsql();
     }
     function test_setorder_argkeyword($t){
-        return $m=$this->add('Model_Book')->setOrder('name','asc')->dsql();
+        return $m=$this->add('Model_Book')->setOrder('name','desc')->dsql();
     }
     function test_setorder_commakeyword($t){
         return $m=$this->add('Model_Book')->setOrder('id desc,isbn,name asc')->dsql();
