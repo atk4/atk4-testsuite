261b2bc3b61e4376702fa793650becff46222435
Romans Malinovskis <r@agiletech.ie>
2013-03-25 23:40:52 +0000

    validator tests

--- page/validator.php
+++ page/validator.php
@@ -19,5 +19,33 @@ class page_validator extends Page_Tester {
         $m->c->is('name|int')->now();
         return 'FAIL';
     }
+    function test_fail($m){
+        $m->c->is('name|fail')->now();
+        return 'FAIL';
+    }
+    function test_eq($m){
+        $m->c->is('name|eq|name')->now();
+        return 'OK';
+    }
+    function test_eq2($m){
+        $m->c->is('name|eq|isbn')->now();
+        return 'OK';
+    }
+    function test_eq3($m){
+        $m->c->is('name|same:isbn')->now();
+        return 'OK';
+    }
+    function test_ne($m){
+        $m->c->is('name|ne|name')->now();
+        return 'OK';
+    }
+    function test_ne2($m){
+        $m->c->is('name|ne|isbn')->now();
+        return 'OK';
+    }
+    function test_ne3($m){
+        $m->c->is('name|different:isbn')->now();
+        return 'OK';
+    }
 }
 
