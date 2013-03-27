9c5bbf6ca61f2e26945ebf01155cfaa22d87ba68
Romans Malinovskis <r@agiletech.ie>
2013-03-27 09:22:28 +0000

    validator

--- page/validator.php
+++ page/validator.php
@@ -7,6 +7,13 @@ class page_validator extends Page_Tester {
         $m->c=$m->add('romaninsh/validation/Controller_Validator');
         return array($m);
     }
+    function executeTest($test_obj,$test_func,$input){
+        try {
+            return parent::executeTest($test_obj,$test_func,$input);
+        } catch (Exception_ValidityCheck $e) {
+            return $e->getMessage();
+        }
+    }
     function test_empty($m){
         $m->c->now();
         return 'OK';
@@ -47,5 +54,27 @@ class page_validator extends Page_Tester {
         $m->c->is('name|different:isbn')->now();
         return 'OK';
     }
+
+    // Next - tests from the README.md
+    function example1($m){
+        $m->c->is(array(
+            'name,surname!|len|5..100',
+            'addr1_postcode|to_alpha|to_trim|uk_zip?Not a UK postcode',
+            'addr2_postcode|if|has_addr1|as|addr1_postcode'
+        ))->now();
+        //$m->hook('beforeSave');
+    }
+
+    function test_exa1($m){
+        $d=array(
+            'name'=>'John',
+            'surname'=>'Smith',
+            'addr1_postcode'=>'E3 3CZ',
+            'has_addr1'=>true,
+            'addr2_postcode'=>'E3 3CZ',
+        );
+        $this->example1($m);
+    }
+
 }
 
